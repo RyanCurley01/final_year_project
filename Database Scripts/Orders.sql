15936b3caf8f8b18480653a57787075499e7308f
A00292817 <ryancurley21@gmail.com>
2025-10-11 21:56:01 +0100

    payments service and music table schema

--- Database Scripts/Orders.sql
+++ Database Scripts/Orders.sql
@@ -23,9 +23,19 @@ CREATE TABLE Order_Items (
 CREATE TABLE Games (
 	-- Unique identifier for each game.
 	GameID INT auto_increment primary key,
-    
 	GameTitle VARCHAR(10),
 	Platform ENUM('PS4','XBOX'),
 	Price decimal(4, 2),
     StockQuantity INT UNSIGNED
+);
+
+CREATE TABLE Music (
+	-- Unique identifier for each musicpurchased_games.
+	musicID INT auto_increment primary key,
+    artist VARCHAR(150),
+    genre VARCHAR(100),
+    album VARCHAR(150),
+    file_url VARCHAR(255),
+    preview_url VARCHAR(255),
+	StockQuantity INT UNSIGNED
 );
