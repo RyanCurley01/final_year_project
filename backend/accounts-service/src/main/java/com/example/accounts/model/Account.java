31a16d5cf14f4b054686667fd429e93f68fc4f47
Ryan Curley <123556848+RyanCurley01@users.noreply.github.com>
2025-11-02 19:11:39 +0000

    comment to added Accont entity class

--- backend/accounts-service/src/main/java/com/example/accounts/model/Account.java
+++ backend/accounts-service/src/main/java/com/example/accounts/model/Account.java
@@ -7,6 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/*
+ * Entity classes for each service represent
+ * the class fields to be mapped to the database columns
+ */
 @Entity
 @Table(name = "Accounts")
 @Data
