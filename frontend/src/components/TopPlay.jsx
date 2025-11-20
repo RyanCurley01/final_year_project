36df1eb58839af43f82aa640b5d78ef1fbf3e182
Ryan Curley <123556848+RyanCurley01@users.noreply.github.com>
2025-11-20 17:17:24 +0000

    Added cover image to TopPlay component songs

--- frontend/src/components/TopPlay.jsx
+++ frontend/src/components/TopPlay.jsx
@@ -20,6 +20,11 @@ const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handle
   hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
+      <img
+        src={song?.albumCoverImageUrl || song?.images?.coverart}
+        alt={song?.albumTitle || song?.title}
+        className="w-20 h-20 rounded-lg"
+      />
       <div className="flex-1 flex flex-col justify-center mx-3">
         <p className="text-xl font-bold text-white">
           {song.albumTitle || song.title}
