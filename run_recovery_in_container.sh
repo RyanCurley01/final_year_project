7816c45ad4e25a878075583acb911c32210d88ef
RyanCurley01 <ryancurley21@gmail.com>
2026-04-15 18:11:45 +0000

    Removed all unused files for testing database and removed hardcoded files with hardcoded credentials

--- run_recovery_in_container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker cp recover_image_generation.py gamestore_services-audio-service-1:/app/recover_image_generation.py
-docker exec -it gamestore_services-audio-service-1 bash -c "pip install boto3 pymysql && python recover_image_generation.py"
